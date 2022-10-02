1457b053bacaa5aa2afccd94192d046b9c9dd524
Youssef Nasr <yoyo.monem22@gmail.com>
2022-10-02 17:13:06 +0200

    Update 2 files

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ const divClassCopyright = document.querySelector(".copyright");
 const footerClassCopyright = document.querySelector("#copyright");
 const divClassHome = document.getElementById("home");
 //#endregion
+//#region All functions
+function youssefLandXScratch(reason, ticks) {
+    console.warn("Redirecting to: https://the-youssef-nasr-company.github.io/Youssef-Land-X-Scratch/ because of " + reason + "in " + ticks + "tick(s)")
+    document.href = "https://the-youssef-nasr-company.github.io/Youssef-Land-X-Scratch/";
+}
+//#endregion
