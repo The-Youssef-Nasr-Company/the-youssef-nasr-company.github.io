7db5de163045e086219353e8c8d62a3c39ffabdd
Youssef Nasr <yoyo.monem22@gmail.com>
2022-10-02 16:59:40 +0200

    Do 2 things

---
+++ src/main.js
@@ -0,0 +1,5 @@
+//#region Elements
+const divClassCopyright = document.querySelector(".copyright");
+const footerClassCopyright = document.querySelector("#copyright");
+const divClassHome = document.getElementById("home");
+//#endregion
