d068ed2c33e1c26dcbfbfcaffb91aae0f845f8cc
Y Nasr <yoyo.monem22@gmail.com>
2022-10-26 17:10:03 +0200

    Update JavaScript functionality of website

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ const footerClassCopyright = document.querySelector("#copyright");
 const divIdHome = document.getElementById("home");
 console.warn("WARNING: Youssef Land X Scratch is no longer on GitHub. It will be brought back, but as a compiler that uses TurboWarp and has a Scratch 2 editor written in NOT JavaScript or Squeak, but Python.");
 function gotoUrl(url) {
-    location.href = url;
+    window.open(url);
 }
