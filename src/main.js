d495a91bbb52438b812867d19f7ff2aea67db179
Y Nasr <yoyo.monem22@gmail.com>
2022-10-26 16:56:04 +0200

    Do 3 things

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ const divClassCopyright = document.querySelector(".copyright");
 const footerClassCopyright = document.querySelector("#copyright");
 const divIdHome = document.getElementById("home");
 console.warn("WARNING: Youssef Land X Scratch is no longer on GitHub. It will be brought back, but as a compiler that uses TurboWarp and has a Scratch 2 editor written in NOT JavaScript or Squeak, but Python.");
+function gotoUrl(url) {
+    location.href = url;
+}
